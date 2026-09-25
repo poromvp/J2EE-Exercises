b76262cc5675598586ebb8e4f7d790269003267c
poromvp <quangkietle382@gmail.com>
2026-09-25 23:34:03 +0700

    change main file and run

--- project5/baitap1-5/baitap1-5/src/main/java/projectj2ee/baitap1_5/Baitap15Application.java
+++ project5/baitap1-5/baitap1-5/src/main/java/projectj2ee/baitap1_5/Baitap15Application.java
@@ -1,5 +1,5 @@
 package projectj2ee.baitap1_5;
-
+import projectj2ee.baitap1_5.controller.PizzaController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Baitap15Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Baitap15Application.class, args);
-	}
+		var context = SpringApplication.run(Baitap15Application.class, args);
 
+		PizzaController pizzaController = context.getBean(PizzaController.class);
+		System.out.println(pizzaController.getPizza());
+	}
 }
