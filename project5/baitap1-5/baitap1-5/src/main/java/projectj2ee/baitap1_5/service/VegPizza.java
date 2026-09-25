6de601a75e4ace10065edce02d2c605ea8e9ce9b
poromvp <quangkietle382@gmail.com>
2026-09-25 23:31:54 +0700

    create VegPizza class

--- project5/baitap1-5/baitap1-5/src/main/java/projectj2ee/baitap1_5/service/VegPizza.java
+++ project5/baitap1-5/baitap1-5/src/main/java/projectj2ee/baitap1_5/service/VegPizza.java
@@ -1,4 +1,10 @@
 package projectj2ee.baitap1_5.service;
+import org.springframework.stereotype.Component;
 
-public class VegPizza {
+@Component("vegPizza")
+public class VegPizza implements Pizza {
+    @Override
+    public String getPizza() {
+        return "Bánh Pizza chay (Veg Pizza) đã sẵn sàng!";
+    }
 }
